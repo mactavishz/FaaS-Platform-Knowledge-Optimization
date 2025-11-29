2f25b4a63a3cbee21c1f77e95ce0ae77ed75df94
mactavishz <maczhanchao@yahoo.com>
2025-11-29 22:13:18 +0100

    Add new function workflow for testing in tinyfaas

---
+++ tests/workflows/tinyfaas/linear/b/handler.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import typing
+import requests
+
+def handle(input: typing.Optional[str], headers: typing.Optional[typing.Dict[str, str]]) -> typing.Optional[str]:
+    res = requests.get("http://tinyfaas.com/fn/linear-c")
+    return {
+        "msg": "Function linear-b is finished",
+        "data": res.json()
+    }
---
+++ tests/workflows/tinyfaas/linear/c/handler.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+import typing
+import requests
+
+def handle(input: typing.Optional[str], headers: typing.Optional[typing.Dict[str, str]]) -> typing.Optional[str]:
+    return {
+        "msg": "Function linear-c is finished",
+        "data": "Hello World! End of the story."
+    }
