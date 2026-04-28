b49fafc6b038dc0edb96f5dc07120f84ac67974e
mactavishz <12948083+mactavishz@users.noreply.github.com>
2026-04-28 20:52:33 +0200

    Remove default REGISTRY_TYPE

--- tests/integration/faasd/workflows_test.go
+++ tests/integration/faasd/workflows_test.go
@@ -24,7 +24,6 @@ func TestWorkflowIntegrationSuite(t *testing.T) {
 func (s *WorkflowIntegrationSuite) SetupSuite() {
 	t := s.T()
 	s.repo = integrationhelpers.RepoRoot(t)
-	t.Setenv("REGISTRY_TYPE", "local")
 	s.baseURL, s.auth = integrationhelpers.RequireFaasd(t)
 }
 
